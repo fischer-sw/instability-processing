ecde621f42f3ee4b328ae9b7cf252e8d6d9694b9
Daniel Fischer <daniel.fischer3@mailbox.tu-dresden.de>
2023-03-14 14:42:21 +0100

    fixed binary images and added animations

--- methods.py
+++ methods.py
@@ -16,16 +16,23 @@ import cv2
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s")
 
 
-def get_image_files(config, case):
+def get_image_files(config, case, folder):
     """
     Function that reads image names from folder
     """
-    dat_path = os.path.join(*config["data_path"], "raw_cases", case)
+    dat_path = os.path.join(*config["data_path"], folder, case)
     if os.path.exists(dat_path) == False:
         logging.warning(f"No data found for case {case} at {dat_path}")
         return []
     images = glob.glob("*.tiff", root_dir=dat_path)
-    tmp_images = [x.split(".")[0] for x in images]
+    if images == []:
+        images = glob.glob("*.png", root_dir=dat_path)
+    if images == []:
+        logging.warning(f"No images found in folder {folder} for case {case}")
+        tmp_images = []
+    else:
+        tmp_images = [x.split(".")[0] for x in images]
+        tmp_images.sort()
     return tmp_images
 
 def get_config():
@@ -43,17 +50,22 @@ def calc_case_ratio():
     """
     config = get_config()
     for cas in list(config["cases"]):
-        images = get_image_files(config, cas)
+        images = get_image_files(config, cas, "raw_cases")
         images.sort()
-    convert2png(config, images, cas)
-    substract_background(config, cas, images)
-    binarize_image(config, cas, images)
+    convert2png(config, cas)
+    substract_background(config, cas)
+    binarize_image(config, cas)
+    create_animation(config, cas, "png_cases")
+    create_animation(config, cas, "binary_cases")
+
     
 
-def convert2png(config, images, case):
+def convert2png(config, case):
     """
     Function that converst .tiff images to .png images and stores them in png_cases folder for a case
     """
+    images = get_image_files(config, case, "raw_cases")
+
     for file in images:
         new_dir_path = os.path.join(*config["data_path"], "png_cases", case)
         new_img_path = os.path.join(new_dir_path, file + ".png")
@@ -78,8 +90,10 @@ def read_image(config, folder, filename, case):
         logging.error(f"Image {img_path} does not exsist")
         exit()
     else:
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE) 
-        # img = cv2.imread(img_path)
+        match folder:
+            case _:
+                img = cv2.imread(img_path)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     return img
 
@@ -98,36 +112,67 @@ def save_image(config, folder, filename, img, case):
         case "binary":
             cv2.imwrite(img_path, img)
         case _:
-            mlt.imsave(img_path, img)
+            # mlt.imsave(img_path, img)
+            cv2.imwrite(img_path, img)
 
-def binarize_image(config, case, images):
+def binarize_image(config, case):
     """
     Function that creates binarize images
     """
+    images = get_image_files(config, case, "background_removed_cases")
+
     for image in images[1:]:
-        new_img_path = os.path.join(*config["data_path"], "binary", case, image + ".png")
+        new_img_path = os.path.join(*config["data_path"], "binary_cases", case, image + ".png")
         if os.path.exists(new_img_path):
             continue
-        tmp_img = read_image(config, "background_removed", image, case)
-        th, im_gray_th_otsu = cv2.threshold(tmp_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        tmp_img = read_image(config, "background_removed_cases", image, case)
+        th, im_gray_th_otsu = cv2.threshold(tmp_img, 128, 255, cv2.THRESH_BINARY |cv2.THRESH_OTSU)
         logging.info(f"Theshold for image {image}: {th}")
-        save_image(config, "binary", image, im_gray_th_otsu, case)
+        pixels, px_count = np.unique(im_gray_th_otsu, return_counts=True)
+        white_ratio = px_count[1]/px_count[0]
+        if white_ratio < 0.1:
+            logging.info(f"White ratio for image {image}: {white_ratio}")
+            logging.warning(f"Binarization failed for image {image}")
+            continue
+        save_image(config, "binary_cases", image, im_gray_th_otsu, case)
 
-def substract_background(config, case, images):
+def substract_background(config, case):
     """
     Function that supstracts background from image
     """
     logging.info(f"Substracting background from images for case {case}")
-
+    images = get_image_files(config, case, "png_cases")
     background = read_image(config, "png_cases", images[0], case)
     for img in images[1:]:
-        new_img_path = os.path.join(*config["data_path"], "background_removed", case, img + ".png")
+        new_img_path = os.path.join(*config["data_path"], "background_removed_cases", case, img + ".png")
         if os.path.exists(new_img_path):
             continue
         tmp_img = read_image(config, "png_cases", img, case)
         img_diff = cv2.subtract(tmp_img, background)
         logging.info(f"Removed backgound for image {img}")
-        save_image(config, "background_removed", img, img_diff, case)
+        save_image(config, "background_removed_cases", img, img_diff, case)
+
+def create_animation(config, case, folder):
+    """
+    Function that creates animation video for images
+    """
+
+    images = get_image_files(config, case, "raw_cases")
+    animation_folder_path = os.path.join(*config["data_path"], "animations", case)
+    dat_path = os.path.join(*config["data_path"], folder, case)
+    frame = cv2.imread(os.path.join(dat_path, images[0]+ ".png"))
+
+    height, width, layers = frame.shape
+    fps = 1000/500
+    video_path = os.path.join(animation_folder_path, folder + ".avi")
+    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'DIVX'), fps, (width, height))
+    for image in images:
+        img = cv2.imread(os.path.join(dat_path, image + ".png"))
+        out.write(img)
+
+    cv2.destroyAllWindows()
+    out.release()
+    logging.info(f"Created {folder} video for case {case}")
 
 if __name__ == "__main__":
     calc_case_ratio()
